cb110acd9e5c6f2b9caf5c081caf79512914646b
Matteo Fortino <matteofortino04@gmail.com>
2020-10-04 22:04:33 +0200

    first working version

---
+++ main.js
@@ -0,0 +1,41 @@
+const square = document.getElementById("square")
+const btn = document.getElementById("btn")
+const playerOneTime = document.getElementById("player-one-time")
+const playerTwoTime = document.getElementById("player-two-time")
+const current = document.getElementById('current-player')
+
+let startTime
+let endTime
+let reactionTime
+let currentPlayer = 'Player One'
+
+
+function startGame() {
+    square.classList.add("bg-red")
+    startTime = new Date().getTime()
+
+    square.addEventListener('click', endGame)
+}
+
+function endGame() {
+    endTime = new Date().getTime()
+    reactionTime = endTime - startTime
+
+    if(currentPlayer == 'Player One') {
+        playerOneTime.innerHTML = reactionTime
+    } else {
+        playerTwoTime.innerHTML = reactionTime
+    }
+
+    square.removeEventListener('click', endGame)
+    square.classList.remove('bg-red')
+    currentPlayer = 'Player Two'
+    current.innerHTML = currentPlayer
+}
+
+btn.addEventListener('click', start)
+
+function start() {
+    setTimeout(startGame, 1000)
+    console.log('ciao')
+}
